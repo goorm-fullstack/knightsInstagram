745fd9945a4e7458ce4f4f07157a22036224c462
Sera Han <hsr0782@gmail.com>
2023-08-11 17:36:13 +0900

    2번째수정

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from 'react';
+import "./EditForm.css"
 
 const EditForm = () => {
   const [profileImage, setProfileImage] = useState('URL_OF_DEFAULT_IMAGE');
@@ -33,38 +34,50 @@ const EditForm = () => {
       <div className="main-content">
         <div className="x9f619">
           <div className="profile-image-container">
-            <img
-              src={profileImage}
-              alt="Profile"
-              className="profile-image"
-              onClick={handleImageChange}
-            />
-          </div>
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label>웹사이트</label>
-              <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            </div>
-            <div className="form-group">
-              <label>소개</label>
-              <textarea value={bio} onChange={(e) => setBio(e.target.value)} />
-            </div>
-            <div className="form-group">
-              <label>성별</label>
-              <input type="text" value={gender} onChange={(e) => setGender(e.target.value)} />
-            </div>
-            <button className="save-button" type="submit">제출</button>
-          </form>
-          {/* 파일 업로드 input */}
-          <input
-            type="file"
-            accept="image/*"
-            onChange={profileChange}
-            ref={profileImgFileInput}
-            style={{ display: 'none' }}
-          />
+           <h2 className="_ac10">프로필 편집</h2>
+           </div>
         </div>
       </div>
+      <div className="_ab4f">
+      <div className="_ab4g">
+       <div className="_aadm">
+       <button className="_aadn" title="프로필 사진 바꾸기" tabIndex="0">
+       <img 
+         alt="프로필 사진 바꾸기"
+         className="_aadp"
+         src="img\profile_basic.png"
+       />
+       </button>
+       </div>
+        <div>
+         <form encType="multipart/form-data" method="POST" role="presentation">
+         <input accept="img\profile_basic.png" className="_ac69" type="file" />
+         </form>
+        </div>
+      </div>
+     <div className="_ab4h">
+      <div className="x1gslohp">
+       <span className="x1lliihq">
+        닉네임
+       </span>
+       </div>
+       <div className="x1i10hfl" role="button">
+         프로필 사진 바꾸기
+       </div>
+       <div>
+         <form encType="multipart/form-data" method="POST" role="presentation">
+         <input accept="img\profile_basic.png" className="_ac69" type="file"/>
+         </form>
+        </div>
+      </div>
+     </div>
+     <form className="_ab43" method="POST">
+      <div className="_ab3p">
+       <aside class="_ad6_">
+        <label class="_ab3q" for="pepLinks">웹사이트</label>
+       </aside>
+      </div>
+     </form>
     </article>
     );
   };
